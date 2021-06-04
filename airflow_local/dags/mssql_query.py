dee63ded991085caccdd930f49597f3da11e5d9a
Paul Fry <paul.fry@versent.com.au>
2021-06-04 15:50:53 +1000

    baselined mssql_query dag

--- airflow_local/dags/mssql_query.py
+++ airflow_local/dags/mssql_query.py
@@ -1,11 +1,28 @@
+#!/usr/bin/env python3
+"""
+Python Version  : 3.7
+* Name          : mssql_query.py
+* Description   : Boilerplate MSSQL query script.
+* Created       : 04-06-2021
+* Usage         : python3 mssql_query.py
+"""
+
+__author__ = "Paul Fry"
+__version__ = "0.1"
 
 import sys
-import os
 import logging
 import pymssql
+from time import time
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
+
+# Set up a specific logger with our desired output level
+logging.basicConfig(format='%(message)s')
+logger = logging.getLogger('airflow.task')
+logger.setLevel(logging.INFO)
 
 default_args = {
     'owner': 'aws',
@@ -15,45 +32,63 @@ default_args = {
 }
 
 dag = DAG(
-        dag_id=os.path.basename(__file__).replace(".py", ""),
-        default_args=default_args,
-        schedule_interval=None
-)
+    'template_mssql_query', default_args=default_args, schedule_interval=None)
 
 def mssql_query(**kwargs):
-    """ function to perform an MSSQL query """
+    """ generic mssql query function """
+    START_TIME = time()
+    logger.debug(f"Function called: mssql_query()")
+
+    db_name, db_schema, ip_tbl_list, db_host, username, password = get_user_ips()
+
     query_result_to_return = ''
-    sql_query = ''
+    sql_query = f"SELECT * FROM {db_name}.{db_schema}.{ip_tbl_list}"
 
     try:
         conn = pymssql.connect(
-            server=f'${HOST}',
-            user=f'${USER}',
-            password=f'${PWD}',
-            database=f'${DB}'
+            server=f'{db_host}',
+            user=f'{username}',
+            password=f'{password}',
+            database=f'{db_name}'
         )
         
         # Create a cursor from the connection
         with conn.cursor() as cursor:
-            logging.info("running query: %s", sql_query)
+            logger.info("running query: %s", sql_query)
 
-            cursor.execute(f"SELECT * from ${DB}.${schema}.${tbl}")
+            cursor.execute(sql_query)
 
             query_result = cursor.fetchall()
             for item in query_result:
                 query_result_to_return += f"{item[1]}\n"
 
-        logging.info("Finished SQL query")
+        logger.info("finished SQL query")
 
     except:
-        logging.error("Error when creating pymssql database connection: %s", sys.exc_info()[0])
+        logger.error("Error when creating pymssql database connection: %s", sys.exc_info()[0])
 
-    logging.debug("Finished mssql_query() function in %s seconds", round(time() - START_TIME, 2))
+    logger.debug(f"Function finished: main() finished in {round(time() - START_TIME, 2)} seconds")
 
     return query_result_to_return
 
-select_query = PythonOperator(
-    task_id='select_query',
+def get_user_ips():
+    """ fetch user inputs """
+    START_TIME = time()
+    logger.debug(f"Function called: get_user_ips()")
+
+    db_name = Variable.get("db_name")
+    db_schema = Variable.get("db_schema")
+    ip_tbl_list = Variable.get("ip_tbl_list")
+    db_host = Variable.get("host")
+    username = Variable.get("username")
+    password = Variable.get("password")
+
+    logger.debug(f"Function finished: get_user_ips() finished in {round(time() - START_TIME, 2)} seconds")
+
+    return db_name, db_schema, ip_tbl_list, db_host, username, password
+
+mssql_select_all_query = PythonOperator(
+    task_id='mssql_select_all_query',
     python_callable=mssql_query,
-    dag=dag,
+    dag=dag
 )
