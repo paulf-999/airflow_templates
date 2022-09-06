68ade3e2a591bb289cfe80eeca8b5f30fda9bffd
Paul Fry <paul-fry@hotmail.com>
2022-09-06 14:07:41 +1000

    resolved install steps

--- dags/active/wip_get_runtime_stats/dag.py
+++ dags/active/wip_get_runtime_stats/dag.py
@@ -46,19 +46,13 @@ dag = DAG(
     schedule_interval=None,
 )
 
-# with DAG(dag_id=dag_name, doc_md=doc_md, default_args=default_args, schedule_interval=None, tags=["template"]) as dag:
-
 ####################################################################
 # DAG Operators
 ####################################################################
 start_task = DummyOperator(task_id="start", dag=dag)
 end_task = DummyOperator(task_id="end", dag=dag)
 
-# TODO
 # get_all_dags
-
-# get_dag_runtime_stats = PythonOperator(task_id="get_dag_run_metadata", python_callable=helpers.get_dag_run_metadata, op_kwargs={"dag_name": "template_dag"})
-
 for selected_dag in helpers.get_dags():
     # print(dag)
 
@@ -70,23 +64,3 @@ for selected_dag in helpers.get_dags():
         # DAG Lineage
         ####################################################################
         start_task >> get_dag_runtime_stats >> end_task
-        # start_task >> end_task
-
-
-if __name__ == "__main__":
-    # helpers.get_dag_run_metadata("template_dag")
-    # helpers.get_dags()
-
-    for my_dag in helpers.get_dags():
-        print(my_dag)
-        if my_dag != dag_name:
-
-            # TODO:
-            # call get dag_run metadata
-            get_dag_run_metadata = PythonOperator(task_id=f"get_dag_run_metadata_for_{my_dag}", python_callable=helpers.get_dag_run_metadata, op_kwargs={"dag_name": my_dag})
-
-            # TODO
-            # get dag_run_task metadata
-
-            # TODO
-            # get DAG metadata
