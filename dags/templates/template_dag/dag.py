8d661551e7f064ad3ec87c1b728f4082ed825e93
Paul Fry <paul-fry@hotmail.com>
2022-10-22 10:36:38 +1100

    added dags/templates. This is a copy of 'active', but might be clearer to others

---
+++ dags/templates/template_dag/dag.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Python Version  : 3.10
+* Name          : template_dag.py
+* Description   : Boilerplate Airflow DAG, fetching attributes from a shared Airflow package
+* Created       : 28-09-2022
+"""
+
+__author__ = "Paul Fry"
+__version__ = "1.0"
+
+from includes import common
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.dummy import DummyOperator
+
+dag_name, dag_helpers, sql_queries, doc_md, logger, local_tz, default_args = common.get_common_dag_vars(__file__)  # noqa
+
+with DAG(dag_id=dag_name, doc_md=doc_md, default_args=default_args, schedule_interval=None, tags=["template"]) as dag:
+
+    ####################################################################
+    # DAG Operators
+    ####################################################################
+    start_task = DummyOperator(task_id="start")
+    end_task = DummyOperator(task_id="end")
+
+    hello_world_task = PythonOperator(task_id="hello_world_task", python_callable=common.hello_world)
+
+####################################################################
+# DAG Lineage
+####################################################################
+start_task >> hello_world_task >> end_task
