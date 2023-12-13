4f616177c6475d06d5122a4add90261008a95f5b
Paul Fry <paul-fry@hotmail.com>
2023-12-13 07:58:41 +0000

    latest tidy up of makefiles

--- src/airflow_w_astro/src/make/setup.mk
+++ src/airflow_w_astro/src/make/setup.mk
@@ -1,38 +1,67 @@
 SHELL = /bin/sh
 
-# Load environment variables from .env file
-include .env
+include .env # Load environment variables from .env file
+include src/make/variables.mk # load variables from a separate file
 
-# load variables from a separate file
-include src/make/variables.mk
-
+TEMPLATES_DIR := src/templates
+JINJA_TEMPLATES_DIR := ${TEMPLATES_DIR}/jinja_templates
+AIRFLOW_DAGS_DIR := ${ASTRO_PROJECT_NAME}/dags
 #=======================================================================
 # Targets
 #=======================================================================
 gen_env_template:
-	@echo && echo "Generate the template .env file"
-	@j2 src/jinja_templates/.env_template.j2 -o .env
+	@echo "${DEBUG}2. Generate template .env file${COLOUR_OFF}"
+	@j2 ${JINJA_TEMPLATES_DIR}/.env_template.j2 -o .env && echo
 
-install:
-	@echo && echo "${INFO}Called makefile target 'install'. Run the setup & install targs.${COLOUR_OFF}" && echo
-	@echo && echo "Step 1: Create an Astro project" && echo
-	@mkdir ${ASTRO_PROJECT_NAME} && cd ${ASTRO_PROJECT_NAME} && astro dev init > /dev/null 2>&1 && touch tests/.gitkeep
-	@echo "Step 2: Generate template airflow_settings.yaml file"
-	@j2 src/templates/jinja_templates/airflow_settings.yaml.j2 -o ${ASTRO_PROJECT_NAME}/airflow_settings.yaml
-	@echo && echo "Step 3: Generate template Dockerfile" && echo
-	@j2 src/templates/jinja_templates/Dockerfile.j2 -o ${ASTRO_PROJECT_NAME}/Dockerfile
-	@echo "Step 4: Copy the generated .env file to the astro project dir" && echo
+create_astro_project:
+	@echo "${DEBUG}1: Create an Astro project${COLOUR_OFF}"
+	@make -s prompt_remove_directory
+	@mkdir ${ASTRO_PROJECT_NAME}
+	@cd ${ASTRO_PROJECT_NAME} && astro dev init > /dev/null 2>&1 && touch tests/.gitkeep
+
+prompt_remove_directory:
+	@if [ -d "${ASTRO_PROJECT_NAME}" ]; then \
+		printf "\nThe target directory '${ASTRO_PROJECT_NAME}' already exists. Do you want to remove it? (y/n): "; \
+		read answer; \
+		if [ "$$answer" != "y" ]; then \
+			printf "${ERROR}\nInstallation aborted - Astronomer project directory already exists.\n${COLOUR_OFF}"; \
+			exit 1; \
+		else \
+			rm -rf "${ASTRO_PROJECT_NAME}" && echo; \
+		fi; \
+	fi
+
+generate_airflow_project_files:
+	@echo "${DEBUG}2. Generate Airflow files.${COLOUR_OFF}"
+	@# Generate template airflow_settings.yaml file
+	@j2 ${JINJA_TEMPLATES_DIR}/airflow_settings.yaml.j2 -o ${ASTRO_PROJECT_NAME}/airflow_settings.yaml
+	@# Generate template Dockerfile
+	@j2 ${JINJA_TEMPLATES_DIR}/Dockerfile.j2 -o ${ASTRO_PROJECT_NAME}/Dockerfile
+	@# Generate the template Makefile
+	@j2 ${JINJA_TEMPLATES_DIR}/Makefile.j2 -o ${ASTRO_PROJECT_NAME}/Makefile && cp config.mk ${ASTRO_PROJECT_NAME}
+
+copy_generated_airflow_files:
+	@echo "${DEBUG}3. Copy generated Airflow files into Astronomer project.${COLOUR_OFF}"
+	@# Copy the generated .env file to the astro project dir
 	@cp .env ${ASTRO_PROJECT_NAME}/.env
-	@echo "Step 5: Copy over the template DAGs to the generated astro project dir" && echo
-	@cp -r src/templates/template_dags/* ${ASTRO_PROJECT_NAME}/dags/
-	@echo "Step 6: Copy the template soda config to the astro project dir" && echo
-	@cp -r src/templates/soda/ ${ASTRO_PROJECT_NAME}/include/
-	@echo "Step 7: Remove the example dags" && echo
-	@rm ${ASTRO_PROJECT_NAME}/dags/example_dag_basic.py && rm ${ASTRO_PROJECT_NAME}/dags/example_dag_advanced.py
-	@echo "Step 8: Generate the template Makefile" && echo
-	@j2 src/templates/jinja_templates/Makefile.j2 -o ${ASTRO_PROJECT_NAME}/Makefile && cp config.mk ${ASTRO_PROJECT_NAME}
+	@# Copy over the template DAGs to the generated astro project dir
+	@cp -r ${TEMPLATES_DIR}/template_dags/* ${AIRFLOW_DAGS_DIR}/
+	@# Copy the template soda config to the astro project dir
+	@cp -r ${TEMPLATES_DIR}/soda/ ${ASTRO_PROJECT_NAME}/include/
+	@# Remove the example dags
+	@rm ${AIRFLOW_DAGS_DIR}/example_dag_basic.py && rm ${AIRFLOW_DAGS_DIR}/example_dag_advanced.py && echo
+
+check_docker:
+	@echo "${DEBUG}Check if Docker is running before proceeding.${COLOUR_OFF}"
+	@if [ "$$(docker info --format '{{.ServerVersion}}' 2>/dev/null)" = "" ]; then \
+		echo "${ERROR}Error: docker is not running. Please launch Docker and try launching Astronomer again.${COLOUR_OFF}"; \
+		exit 1; \
+	else \
+		echo "${DEBUG}Docker is running.${COLOUR_OFF}"; \
+	fi
+
 
 # Phony targets
-.PHONY: gen_env_template create_svc_cicd_user_and_role get_ips
+.PHONY: gen_env_template create_astro_project prompt_remove_directory generate_airflow_project_files copy_generated_airflow_files check_docker
 # .PHONY tells Make that these targets don't represent files
 # This prevents conflicts with any files named "all" or "clean"
